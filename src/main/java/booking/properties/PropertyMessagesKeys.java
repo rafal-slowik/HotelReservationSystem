0448b85c60a7a6ad8c9510f88874d325630f5e0b
Rafal Slowik <rafal2308@gmail.com>
2017-03-13 08:56:34 +0000

    Changed class name

--- src/main/java/booking/properties/PropertyMessagesKeys.java
+++ src/main/java/booking/properties/PropertyMessagesKeys.java
@@ -8,13 +8,13 @@ package booking.properties;
  * @date 11 Mar 2017
  *
  */
-public enum ExceptionMessagesKeys {
+public enum PropertyMessagesKeys {
 	EXC_MSG_ROOM_ALREADY_BOOKED("exception.room_already_booked.text"), //
 	EXC_MSG_ROOM_DOES_NOT_EXIST("exception.room_does_not_exist.text"), //
 	EXC_MSG_PARAM_MUST_NOT_BE_NULL("exception.must_not_be_null.text"), //
 	EXC_MSG_REP_TYPE_NOT_SUPPORTED("exception.repository_type_not_supported.text");
 
-	private ExceptionMessagesKeys(String propertyKey) {
+	private PropertyMessagesKeys(String propertyKey) {
 		this.propertyKey = propertyKey;
 	}
 
